34b76a5aa854bffc95ac902907d2cabbc1e29eba
yuanff <yuanff19@139.com>
2015-08-13 14:07:33 +0800

    Create new file

---
+++ Config.php
@@ -0,0 +1,16 @@
+<?php
+//数据库连接
+define("DB_DNS","mysql:host=127.0.0.1;dbname=demoserver");
+define("DB_USER","xxxx");
+define("DB_PASSWORD","xxxxxxxx");
+
+//融云API地址
+define("RONGCLOUD_API_URL","http://api.cn.rong.io");
+//融云APP KEY
+define("RONGCLOUD_APP_KEY","xxxxxxxxxxxxx");
+//融云APP SECRET
+define("RONGCLOUD_APP_SECRET","TESTSECRET");
+//登录加密秘钥
+define("AUTH_COOKIE_KEY","PHPAUTH");
+//登录加密秘钥
+define("AUTH_SECRET_KEY","xxxxxxxx");
