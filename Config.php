22681d41d31307b056186f74017f568f5f7e0420
yuanff <yuanff19@139.com>
2015-08-19 16:23:56 +0800

    Update Config.php

--- Config.php
+++ Config.php
@@ -7,9 +7,9 @@ define("DB_PASSWORD","xxxxxxxx");
 //融云API地址
 define("RONGCLOUD_API_URL","http://api.cn.rong.io");
 //融云APP KEY
-define("RONGCLOUD_APP_KEY","xxxxxxxxxxxxx");
+define("RONGCLOUD_APP_KEY","25wehl3uwrumw");
 //融云APP SECRET
-define("RONGCLOUD_APP_SECRET","TESTSECRET");
+define("RONGCLOUD_APP_SECRET","zLB2FFLGPSc3");
 //登录加密秘钥
 define("AUTH_COOKIE_KEY","PHPAUTH");
 //登录加密秘钥
